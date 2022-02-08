38ec76dfba5e7ce30b4ad15c83b8449b9fd27954
lenus254 <linus.cheruiyot@student.moringaschool.com>
2022-02-08 07:22:27 +0300

    created comment view in main view

--- app/main/views.py
+++ app/main/views.py
@@ -8,7 +8,7 @@
 from flask import render_template,request,redirect,url_for,abort
 from . import main
 from ..models import Comment, User, Pitch
-from .forms import PitchForm #,CommentForm,UpdateProfile
+from .forms import PitchForm ,CommentForm,UpdateProfile
 from flask_login import login_required, current_user
 from .. import db,photos
 
@@ -44,3 +44,20 @@ def new_pitch():
         return redirect(url_for('main.index'))
         
     return render_template('create_pitch.html', form = pform)
+
+#comments view function
+
+@main.route('/comment/<int:pitch_id>', methods = ['POST','GET'])
+@login_required
+def comment(pitch_id):
+    form = CommentForm()
+    pitch = Pitch.query.get(pitch_id)
+    all_comments = Comment.query.filter_by(pitch_id = pitch_id).all()
+    if form.validate_on_submit():
+        comment = form.comment.data 
+        pitch_id = pitch_id
+        user_id = current_user._get_current_object().id
+        new_comment = Comment(comment = comment,user_id = user_id,pitch_id = pitch_id)
+        new_comment.save_c()
+        return redirect(url_for('.comment', pitch_id = pitch_id))
+    return render_template('comment.html', form =form, pitch = pitch,all_comments=all_comments)
